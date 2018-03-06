ca678eaf907ae2ffa87cb230712447df4e2cf002
unknown <romain.montgrandi@ynov.com>
2018-03-06 11:02:18 +0100

    kdo

---
+++ src/model/Hero.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Hero {
+
+	String name;
+	Number life;
+	Number maxLife;
+	Number stamina;
+	Number maxStamina;
+
+	public Hero(){
+		this.name = "Ynovator";
+	}
+	public Hero(String name){
+
+	}
+}
